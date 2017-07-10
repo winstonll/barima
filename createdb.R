b53d13ec07d3f8e8a2a9a7266af08bf27086b60f
winstonl <winstonbcom.li@mail.utoronto.ca>
2017-07-10 12:25:47 -0400

    updated createdb

--- createdb.R
+++ createdb.R
@@ -4,29 +4,30 @@ library(RMySQL)
 library(SocialMediaMineR)
 
 project = 'unified-sensor-173013'
-#project = 'phd-media-1489418588611'
 sql = "SELECT created_utc, subreddit, domain, url, num_comments,
         title, selftext 
         FROM [fh-bigquery:reddit_posts.2016_01] 
         WHERE subreddit in ('todayilearned', 'science', 'worldnews', 
-        'movies', 'music', 'news', 'books', 'space', 'gadgets', 
-        'technology', 'politics', 'games', 'economics', )
+        'movies', 'Music', 'news', 'books', 'space', 'gadgets', 
+        'technology', 'politics', 'Games', 'Economics', 'relationships',
+        'Fitness', 'Bitcoin', 'lgbt', 'writing', 'Android', 'PS4', 'nyc', 'LosAngeles',
+        'toronto', 'KotakuInAction')
         AND selftext <> '[removed]' 
         AND selftext <> '[deleted]'
-        LIMIT 100"
+        "
 
 data_raw = query_exec(sql, project = project, max_pages = Inf)
-data_raw$paragraph_count = 0
+data_raw$paragraph_count = 0L
 data_raw$lexical_diversity = 0
-data_raw$img_count = 0
+data_raw$img_count = 0L
 data_raw$descriptive_words = 0
-data_raw$video_count = 0
+data_raw$video_count = 0L
 data_raw$reading_time = 0
 data_raw$sentiment = 0
 data_raw$reading_difficulty = 0
 #data_raw$fb_share = 0
 #data_raw$fb_comments = 0
-data_raw$linkedin_shares = 0
+data_raw$linkedin_shares = 0L
 
 
 index = rep(1, nrow(data_raw))
@@ -39,7 +40,7 @@ for(i in 1:nrow(data_raw)){
 
         if(class(x) == 'try-error')
             index[i] = 0
-        else if(http_status(x)$category != 'Success')
+        else if(x$status_code != 200)
             index[i] = 0
         else{
             #fb_table = get_facebook(data_raw$url[i])
@@ -47,7 +48,7 @@ for(i in 1:nrow(data_raw)){
             #data_raw$fb_share[i] = fb_table[2]
             #data_raw$fb_comments[i] = fb_table[1]
             if(class(linkedin_table) != 'try-error')
-                data_raw$linkedin_shares[i] = linkedin_table[1]
+                data_raw$linkedin_shares[i] = as.integer(linkedin_table[1])
         }
      }
     print(i)
