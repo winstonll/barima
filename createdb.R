899e7793bfa95435ef3828ea9c64ae75ef913873
winstonl <winstonbcom.li@mail.utoronto.ca>
2017-07-12 15:39:49 -0400

    new approach to createdb

--- createdb.R
+++ createdb.R
@@ -3,56 +3,69 @@ library(httr)
 library(RMySQL)
 library(SocialMediaMineR)
 
-project = 'unified-sensor-173013'
-sql = "SELECT created_utc, subreddit, domain, url, num_comments,
-        score, title, selftext 
-        FROM [fh-bigquery:reddit_posts.2016_01] 
-        WHERE subreddit in ('todayilearned', 'science', 'worldnews', 
+subs = c('todayilearned', 'science', 'worldnews', 
         'movies', 'Music', 'news', 'books', 'space', 'gadgets', 
         'technology', 'politics', 'Games', 'Economics', 'relationships',
         'Fitness', 'Bitcoin', 'lgbt', 'writing', 'Android', 'PS4', 'nyc', 'LosAngeles',
         'toronto', 'KotakuInAction')
-        AND selftext <> '[removed]' 
-        AND selftext <> '[deleted]'"
+dates = c('2016_01', '2016_02', '2016_03', '2016_04', '2016_05', '2016_06', '2016_07', 
+            '2016_08', '2016_09', '2016_10', '2016_11', '2016_12', '2017_01', '2017_02', 
+            '2017_03', '2017_04', '2017_05')
 
-data_raw = query_exec(sql, project = project, max_pages = Inf)
-data_raw$paragraph_count = 0L
-data_raw$lexical_diversity = 0
-data_raw$img_count = 0L
-data_raw$descriptive_words = 0
-data_raw$video_count = 0L
-data_raw$reading_time = 0
-data_raw$sentiment = 0
-data_raw$reading_difficulty = 0
-data_raw$linkedin_shares = 0L
-data_raw$keywords = ''
-
-dim = nrow(data_raw)
-index = rep(1, dim)
-
-for(i in 1:dim){
-    if(substr(data_raw$domain[i], 1, 5) == 'self.' & nchar(data_raw$selftext[i]) < 100)
-        index[i] = 0
-    else{
-        x = try(GET(data_raw$url[i]), silent = T)
-        if(class(x) == 'try-error')
-            index[i] = 0
-        else if(x$status_code != 200)
-            index[i] = 0
-        else{
-            linkedin_table = try(get_linkedin(data_raw$url[i]), silent = T)
-            if(class(linkedin_table) != 'try-error')
-                data_raw$linkedin_shares[i] = as.integer(linkedin_table[1])
-        }
-     }
-    if(i %% 100 == 0)
-        print(i, dim)
-}
-
-data = data_raw[as.logical(index), ]
+project = 'unified-sensor-173013'
 
 con = dbConnect(MySQL(), user = 'winstonl', password = '111111', 
             host = '146.148.45.182',
             dbname = 'arimadb')
 
-dbWriteTable(con, 'reddit_posts', data, append = T)
+for(d in dates){
+    for(s in subs){
+        sql = paste("SELECT created_utc, subreddit, domain, url, num_comments,
+                score, title, selftext 
+                FROM [fh-bigquery:reddit_posts.", d, "] 
+                WHERE subreddit in ('", s, "')
+                AND selftext <> '[removed]' 
+                AND selftext <> '[deleted]'
+                AND score > 49", sep = '')
+
+        data_raw = query_exec(sql, project = project, max_pages = Inf)
+        data_raw$paragraph_count = 0L
+        data_raw$lexical_diversity = 0
+        data_raw$img_count = 0L
+        data_raw$descriptive_words = 0
+        data_raw$video_count = 0L
+        data_raw$reading_time = 0
+        data_raw$sentiment = 0
+        data_raw$reading_difficulty = 0
+        data_raw$linkedin_shares = 0L
+        data_raw$keywords = ''
+
+        dim = nrow(data_raw)
+        index = rep(1, dim)
+
+        for(i in 1:dim){
+            if(substr(data_raw$domain[i], 1, 5) == 'self.' & nchar(data_raw$selftext[i]) < 100)
+                index[i] = 0
+            else{
+                x = try(GET(data_raw$url[i]), silent = T)
+                if(class(x) == 'try-error')
+                    index[i] = 0
+                else if(x$status_code != 200)
+                    index[i] = 0
+                else{
+                    linkedin_table = try(get_linkedin(data_raw$url[i]), silent = T)
+                    if(class(linkedin_table) != 'try-error')
+                        data_raw$linkedin_shares[i] = as.integer(linkedin_table[1])
+                }
+             }
+            if(i %% 100 == 0)
+                print(paste(i, dim, sep = ', '))
+        }
+
+        data = data_raw[as.logical(index), ]
+
+        dbWriteTable(con, 'reddit_posts', data, append = T)
+        print(s)
+        print(d)
+    }
+}
