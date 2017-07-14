d66968796eec0e2316a536cdaf6c183881ae646d
winstonl <winstonbcom.li@mail.utoronto.ca>
2017-07-14 16:18:24 -0400

    more changes :((((

--- createdb.R
+++ createdb.R
@@ -9,11 +9,13 @@ subs = c('todayilearned', 'science', 'worldnews',
         'Fitness', 'Bitcoin', 'lgbt', 'writing', 'Android', 'PS4', 'nyc', 'LosAngeles',
         'toronto', 'KotakuInAction')
 
-# dates = c('2016_01', '2016_02', '2016_03', '2016_04', '2016_05', '2016_06', '2016_07', 
-#             '2016_08', '2016_09', '2016_10', '2016_11', '2016_12', '2017_01', '2017_02', 
-#             '2017_03', '2017_04', '2017_05')
+size = c(1,1,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1)
 
-dates = '2016_02'
+num = length(size)
+
+dates = c('2016_01', '2016_02', '2016_03', '2016_04', '2016_05', '2016_06', '2016_07', 
+            '2016_08', '2016_09', '2016_10', '2016_11', '2016_12', '2017_01', '2017_02', 
+            '2017_03', '2017_04', '2017_05')
 
 project = 'unified-sensor-173013'
 
@@ -27,15 +29,27 @@ get_linkedin = function (links){
 }
 
 for(d in dates){
-    for(s in subs){
-        print(paste("Start", d, s, Sys.time(), sep = ', '))
-        sql = paste("SELECT created_utc, subreddit, domain, url, num_comments,
-                score, title, selftext 
-                FROM [fh-bigquery:reddit_posts.", d, "] 
-                WHERE subreddit in ('", s, "')
-                AND selftext <> '[removed]' 
-                AND selftext <> '[deleted]'
-                AND score > 49", sep = '')
+    for(s in 1:num){
+        print(paste("Start", d, subs[s], Sys.time(), sep = ', '))
+        if(size[s] == 1){
+            sql = paste("SELECT created_utc, subreddit, domain, url, num_comments,
+                    score, title, selftext 
+                    FROM [fh-bigquery:reddit_posts.", d, "] 
+                    WHERE subreddit in ('", subs[s], "')
+                    AND selftext <> '[removed]' 
+                    AND selftext <> '[deleted]'
+                    AND (score > 99 OR num_comments > 200)", sep = '')
+        }
+
+        else{
+                sql = paste("SELECT created_utc, subreddit, domain, url, num_comments,
+                    score, title, selftext 
+                    FROM [fh-bigquery:reddit_posts.", d, "] 
+                    WHERE subreddit in ('", subs[s], "')
+                    AND selftext <> '[removed]' 
+                    AND selftext <> '[deleted]'
+                    AND (score > 49 OR num_comments > 100)", sep = '')
+        }
 
         data_raw = query_exec(sql, project = project, max_pages = Inf)
         data_raw$paragraph_count = 0L
@@ -50,34 +64,37 @@ for(d in dates){
         data_raw$keywords = ''
 
         dim = nrow(data_raw)
-        index = rep(1, dim)
+        index = rep(0, dim)
 
-        for(i in 1:dim){
-            if(substr(data_raw$domain[i], 1, 5) == 'self.' & nchar(data_raw$selftext[i]) < 100)
-                index[i] = 0
+        for(i in 2250:2300){
+            if(substr(data_raw$domain[i], 1, 5) == 'self.')
+                if(nchar(data_raw$selftext[i]) > 100)
+                    index[i] = 1
+                else
+                    next
             else{   
                 shorten = strsplit(data_raw$url[i], '#')[[1]][1]
-                x = evalWithTimeout(try(GET(shorten), silent = T), timeout = 10, onTimeout = "silent") 
-                if(exists('x'))
-                    index[i] = 0
-                else if(class(x) == 'try-error')
-                    index[i] = 0
+                x = evalWithTimeout(try(GET(shorten), silent = T), timeout = 10, onTimeout = "error") 
+                if(class(x) == 'try-error')
+                    next
                 else if(x$status_code == 200){
                     linkedin_table = get_linkedin(shorten)
                     data_raw$linkedin_shares[i] = as.integer(linkedin_table[1])
+                    index[i] = 1
                 }
-             }
+            }
             if(i %% 1 == 0)
                 print(paste(i, dim, Sys.time(), sep = ', '))
         }
 
         data = data_raw[as.logical(index), ]
-
+        data$created_utc = as.POSIXct(data$created_utc, origin='1970-01-01')
         con = dbConnect(MySQL(), user = 'winstonl', password = '111111', 
             host = '146.148.45.182',
             dbname = 'arimadb')
         
         dbWriteTable(con, 'reddit_posts', data, append = T)
+        print(nrow(data), 'Columns written to DB', sep = ' ')
         dbDisconnect(con)
         print(paste('End', d, s, Sys.time(), sep = ', '))
     }
