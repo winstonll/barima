c40585227cbc164d8320ce0aac521c2b5e602a70
winstonl <winstonbcom.li@mail.utoronto.ca>
2017-07-27 15:27:08 -0400

    minor sql fix

--- encoding.sql
+++ encoding.sql
@@ -1,3 +1,5 @@
+mysql --host=146.148.45.182 --user=winstonl --password=111111
+
 SELECT COLUMN_NAME, CHARACTER_SET_NAME, CHARACTER_MAXIMUM_LENGTH FROM INFORMATION_SCHEMA.COLUMNS 
   WHERE table_name = 'reddit_posts';
 
