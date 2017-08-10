8ff3bc10c1a520c4e68ef52de1ed70d62c0f1530
winstonl <winstonbcom.li@mail.utoronto.ca>
2017-08-10 16:31:52 -0400

    more stuff incomplete

--- extract.py
+++ extract.py
@@ -1,6 +1,7 @@
 from urlAnalyzer import URLAnalyzer
 from textAnalyzer import TextAnalyzer
 import pymysql.cursors
+import pandas as pd
 
 connection = pymysql.connect(host = '146.148.45.182', user = 'winstonl', password = '111111', db = 'arimadb',
     charset = 'utf8mb4', cursorclass = pymysql.cursors.DictCursor)
@@ -11,50 +12,62 @@ for i in range(1, 242895):
     sql = "SELECT * FROM reddit_posts LIMIT 1 OFFSET %s"
     cursor.execute(sql, (i-1))
     print('Currently working on id: ' + str(i))
-    row = cursor.fetchone()
+    row = cursor.fetchall()
     id = row['row_names']
-    if row['selftext'] == '':
-        print('This is a link')
-        analyzer = URLAnalyzer(row['url'])
-        text = analyzer.text
-        par_count = analyzer.paragraph_counter()
-        lex = analyzer.lexical_diversity()
-        nword = analyzer.nword
-        read_time = analyzer.read_time()
-        img = analyzer.count_img()
-        grade, word_usage_freq = analyzer.reading_difficulty()
-        descriptive = analyzer.descriptive_words_porp()
-        vid = analyzer.count_video()
-        sentiment = analyzer.sentiment()
-        original_title = analyzer.original_title
-        title_analyzer = TextAnalyzer(original_title)
-        title_nchar = len(original_title)
-        title_nword = title_analyzer.nword
-        title_sentiment = title_analyzer.sentiment()
-        sql_update = 'UPDATE reddit_posts SET selftext = %s, paragraph_count = %s, lexical_diversity = %s, img_count = %s, \
-            descriptive_words = %s, video_count = %s, reading_time = %s, sentiment = %s, reading_difficulty = %s, \
-            original_title = %s, title_nchar = %s, title_nword = %s, title_sentiment = %s WHERE row_names = %s;'
-        rowUpdate = cursor.execute(sql_update, (text, par_count, lex, img, descriptive, vid, read_time, sentiment, grade, \
-            original_title, title_nchar, title_nword, title_sentiment, id))
-        connection.commit()
-    else:
-        print("This is self post")
-        analyzer = TextAnalyzer(row['selftext'])
-        par_count = analyzer.paragraph_counter()
-        lex = analyzer.lexical_diversity()
-        nword = analyzer.nword
-        read_time = analyzer.read_time()
-        grade, word_usage_freq = analyzer.reading_difficulty()
-        descriptive = analyzer.descriptive_words_porp()
-        sentiment = analyzer.sentiment()
-        title_analyzer = TextAnalyzer(row['title'])
-        title_nchar = len(row['title'])
-        title_nword = title_analyzer.nword
-        title_sentiment = title_analyzer.sentiment()
+    df = pd.DataFrame(row)
+    for row in df
+        if row['selftext'] == '':
+            print('This is a link')
+            analyzer = URLAnalyzer(row['url'])
+            row['selftext'] = analyzer.text
+            row['paragraph_count'] = analyzer.paragraph_counter()
+            row['lexical_diversity'] = analyzer.lexical_diversity()
+            nword = analyzer.nword
+            read_time = analyzer.read_time()
+            img = analyzer.count_img()
+            grade, word_usage_freq = analyzer.reading_difficulty()
+            descriptive = analyzer.descriptive_words_porp()
+            vid = analyzer.count_video()
+            sentiment = analyzer.sentiment()
+            original_title = analyzer.original_title
+            title_analyzer = TextAnalyzer(original_title)
+            title_nchar = len(original_title)
+            title_nword = title_analyzer.nword
+            title_sentiment = title_analyzer.sentiment()
+            sql_update = 'UPDATE reddit_posts SET selftext = %s, paragraph_count = %s, lexical_diversity = %s, img_count = %s, \
+                descriptive_words = %s, video_count = %s, reading_time = %s, sentiment = %s, reading_difficulty = %s, \
+                original_title = %s, title_nchar = %s, title_nword = %s, title_sentiment = %s WHERE row_names = %s;'
+            rowUpdate = cursor.execute(sql_update, (text, par_count, lex, img, descriptive, vid, read_time, sentiment, grade, \
+                original_title, title_nchar, title_nword, title_sentiment, id))
+            connection.commit()
+        else:
+            print("This is self post")
+            analyzer = TextAnalyzer(row['selftext'])
+            par_count = analyzer.paragraph_counter()
+            lex = analyzer.lexical_diversity()
+            nword = analyzer.nword
+            read_time = analyzer.read_time()
+            grade, word_usage_freq = analyzer.reading_difficulty()
+            descriptive = analyzer.descriptive_words_porp()
+            sentiment = analyzer.sentiment()
+            title_analyzer = TextAnalyzer(row['title'])
+            title_nchar = len(row['title'])
+            title_nword = title_analyzer.nword
+            title_sentiment = title_analyzer.sentiment()
 
-        sql_update = 'UPDATE reddit_posts SET paragraph_count = %s, lexical_diversity = %s, \
-            descriptive_words = %s, reading_time = %s, sentiment = %s, reading_difficulty = %s, \
-            title_nchar = %s, title_nword = %s, title_sentiment = %s WHERE row_names = %s;'
-        rowUpdate = cursor.execute(sql_update, (par_count, lex, descriptive, read_time, sentiment, grade, \
-            title_nchar, title_nword, title_sentiment, id))
-        connection.commit()
+            sql_update = 'UPDATE reddit_posts SET paragraph_count = %s, lexical_diversity = %s, \
+                descriptive_words = %s, reading_time = %s, sentiment = %s, reading_difficulty = %s, \
+                title_nchar = %s, title_nword = %s, title_sentiment = %s WHERE row_names = %s;'
+            rowUpdate = cursor.execute(sql_update, (par_count, lex, descriptive, read_time, sentiment, grade, \
+                title_nchar, title_nword, title_sentiment, id))
+            connection.commit()
+
+
+
+df = pd.DataFrame({ 'A' : 1.,
+    'B' : pd.Timestamp('20130102'),
+    'D' : np.array([3] * 4,dtype='int32'),
+    'E' : pd.Categorical(["test","train","test","train"]),
+    'F' : 'foo' })
+
+df = pd.DataFrame(row)
