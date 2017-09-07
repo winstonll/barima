39fe9a792834f83bc7f7aadad3d23c4b026d54f6
winstonl <winstonbcom.li@mail.utoronto.ca>
2017-09-06 20:29:12 -0400

    get back to it

--- extract.py
+++ extract.py
@@ -1,27 +1,25 @@
 from urlAnalyzer import URLAnalyzer
 from textAnalyzer import TextAnalyzer
-import pymysql.cursors
+from sqlalchemy import create_engine
 import pandas as pd
 
-connection = pymysql.connect(host = '146.148.45.182', user = 'winstonl', password = '111111', db = 'arimadb',
-    charset = 'utf8mb4', cursorclass = pymysql.cursors.DictCursor)
+# connection = pymysql.connect(host = '146.148.45.182', user = 'winstonl', password = '111111', db = 'arimadb',
+#     charset = 'utf8mb4', cursorclass = pymysql.cursors.DictCursor)
 
-cursor = connection.cursor()
+# cursor = connection.cursor()
 
-for i in range(1, 242895):
-    sql = "SELECT * FROM reddit_posts LIMIT 1 OFFSET %s"
-    cursor.execute(sql, (i-1))
-    print('Currently working on id: ' + str(i))
-    row = cursor.fetchall()
-    id = row['row_names']
-    df = pd.DataFrame(row)
-    for row in df
+engine = create_engine('mysql+pymysql://winstonl:111111@146.148.45.182:3306/arimadb')
+sql_template = "SELECT * FROM reddit_posts LIMIT 1 OFFSET %s"
+
+with engine.connect() as conn, conn.begin():
+    for i in range(1, 242895):
+        sql = sql_template % (i-1)
+        row = pd.read_sql(sql, conn)
         if row['selftext'] == '':
-            print('This is a link')
-            analyzer = URLAnalyzer(row['url'])
-            row['selftext'] = analyzer.text
-            row['paragraph_count'] = analyzer.paragraph_counter()
-            row['lexical_diversity'] = analyzer.lexical_diversity()
+            analyzer = URLAnalyzer(row['url'][0])
+            selftext = analyzer.text.encode('utf-8')
+            paragraph_count = analyzer.paragraph_counter()
+            lexical_diversity = analyzer.lexical_diversity()
             nword = analyzer.nword
             read_time = analyzer.read_time()
             img = analyzer.count_img()
@@ -34,15 +32,19 @@ for i in range(1, 242895):
             title_nchar = len(original_title)
             title_nword = title_analyzer.nword
             title_sentiment = title_analyzer.sentiment()
-            sql_update = 'UPDATE reddit_posts SET selftext = %s, paragraph_count = %s, lexical_diversity = %s, img_count = %s, \
-                descriptive_words = %s, video_count = %s, reading_time = %s, sentiment = %s, reading_difficulty = %s, \
-                original_title = %s, title_nchar = %s, title_nword = %s, title_sentiment = %s WHERE row_names = %s;'
-            rowUpdate = cursor.execute(sql_update, (text, par_count, lex, img, descriptive, vid, read_time, sentiment, grade, \
-                original_title, title_nchar, title_nword, title_sentiment, id))
-            connection.commit()
+
+            d = pd.DataFrame({'created_at': row['created_utc'][0], 'subreddit': row['subreddit'][0], 
+                'url': row['url'][0], 'num_comments': row['num_comments'][0], 'score': row['score'][0], 
+                'title': row['title'][0], 'selftext': selftext, 'paragraph_count': paragraph_count, 'image_count': img,
+                'video_count': vid, 'reading_time': read_time, 'sentiment': sentiment, 'reading_difficulty': grade, 
+                'word_freq': word_usage_freq,'descriptive_words': descriptive,
+                'linkedin_shares': row['linkedin_shares'][0], 'keywords': '', 'original_title': original_title,
+                'title_nchar': title_nchar, 'title_nword': title_nword, 'title_sentiment': title_sentiment}, index=[0])
+            d.to_sql('reddit_data', conn, if_exists = 'append', index = False)
+        
         else:
             print("This is self post")
-            analyzer = TextAnalyzer(row['selftext'])
+            analyzer = TextAnalyzer(row['selftext'][0])
             par_count = analyzer.paragraph_counter()
             lex = analyzer.lexical_diversity()
             nword = analyzer.nword
@@ -50,24 +52,17 @@ for i in range(1, 242895):
             grade, word_usage_freq = analyzer.reading_difficulty()
             descriptive = analyzer.descriptive_words_porp()
             sentiment = analyzer.sentiment()
-            title_analyzer = TextAnalyzer(row['title'])
-            title_nchar = len(row['title'])
+            title_analyzer = TextAnalyzer(row['title'][0])
+            title_nchar = len(row['title'][0])
             title_nword = title_analyzer.nword
             title_sentiment = title_analyzer.sentiment()
 
-            sql_update = 'UPDATE reddit_posts SET paragraph_count = %s, lexical_diversity = %s, \
-                descriptive_words = %s, reading_time = %s, sentiment = %s, reading_difficulty = %s, \
-                title_nchar = %s, title_nword = %s, title_sentiment = %s WHERE row_names = %s;'
-            rowUpdate = cursor.execute(sql_update, (par_count, lex, descriptive, read_time, sentiment, grade, \
-                title_nchar, title_nword, title_sentiment, id))
-            connection.commit()
+            d = pd.DataFrame({'created_at': row['created_utc'][0], 'subreddit': row['subreddit'][0], 
+                'url': row['url'][0], 'num_comments': row['num_comments'][0], 'score': row['score'][0], 
+                'title': row['title'][0], 'selftext': selftext, 'paragraph_count': paragraph_count, 'image_count': 0,
+                'video_count': 0, 'reading_time': read_time, 'sentiment': sentiment, 'reading_difficulty': grade, 
+                'word_freq': word_usage_freq,'descriptive_words': descriptive,
+                'linkedin_shares': row['linkedin_shares'][0], 'keywords': '', 'original_title': '',
+                'title_nchar': title_nchar, 'title_nword': title_nword, 'title_sentiment': title_sentiment}, index=[0])
 
-
-
-df = pd.DataFrame({ 'A' : 1.,
-    'B' : pd.Timestamp('20130102'),
-    'D' : np.array([3] * 4,dtype='int32'),
-    'E' : pd.Categorical(["test","train","test","train"]),
-    'F' : 'foo' })
-
-df = pd.DataFrame(row)
+        d.to_sql('reddit_data', conn, if_exists = 'append', index = False)
